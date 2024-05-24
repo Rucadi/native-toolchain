f0599b899daf1730dde98890e1929543420dd4ce
rucadi <ruben.cano96@gmail.com>
2024-05-24 18:49:54 +0200

    Track hashes using flake.lock, patch toolchain for sdl2-config, source the environment-setup instead of aliasing cmake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,30 @@
 {
   description = "Cmake with webOS toolchain";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; 
+    x86_64-linux = {
+      url = "file+https://github.com/openlgtv/buildroot-nc4/releases/latest/download/arm-webos-linux-gnueabi_sdk-buildroot.tar.gz";
+      flake = false;
+    };
 
-  outputs = { self, nixpkgs }:
+    aarch64-linux = {
+      url = "file+https://github.com/webosbrew/native-toolchain/releases/latest/download/arm-webos-linux-gnueabi_sdk-buildroot_linux-aarch64.tar.bz2";
+      flake = false;
+    };
+
+    x86_64-darwin = {
+      url = "file+https://github.com/webosbrew/native-toolchain/releases/latest/download/arm-webos-linux-gnueabi_sdk-buildroot_darwin-x86_64.tar.bz2";
+      flake = false;
+    };
+
+    aarch64-darwin = {
+      url = "file+https://github.com/webosbrew/native-toolchain/releases/latest/download/arm-webos-linux-gnueabi_sdk-buildroot_darwin-arm64.tar.bz2";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ...}@inputs:
     let
       allSystems = [
         "x86_64-linux" # 64bit AMD/Intel x86
@@ -17,29 +38,19 @@
         (system: fn { pkgs = import nixpkgs { inherit system; }; inherit system;});
 
       webOSToolchain = {system, fetchurl, runCommand}: let 
-          urlToolchain = {
-              "x86_64-linux"  = "https://github.com/openlgtv/buildroot-nc4/releases/download/webos-d7ed7ee/arm-webos-linux-gnueabi_sdk-buildroot.tar.gz";
-              "aarch64-linux" = "https://github.com/webosbrew/native-toolchain/releases/download/webos-d7ed7ee.6/arm-webos-linux-gnueabi_sdk-buildroot_linux-aarch64.tar.bz2";
-              "x86_64-darwin" = "https://github.com/webosbrew/native-toolchain/releases/download/webos-d7ed7ee.6/arm-webos-linux-gnueabi_sdk-buildroot_darwin-x86_64.tar.bz2";
-              "aarch64-darwin"= "https://github.com/webosbrew/native-toolchain/releases/download/webos-d7ed7ee.6/arm-webos-linux-gnueabi_sdk-buildroot_darwin-arm64.tar.bz2";
-          }."${system}";
-
-          hashToolchain = {
-              "x86_64-linux"  = "sha256-MoFmJumfs0kipJ0MY598ijA1b/+yIjctSCMCfxOC9kA=";
-              "aarch64-linux" = "sha256-RaLRL/VXRX2SzeT92qd6bxCQ/KA63EO7dDl+Xgw3lQE=";
-              "x86_64-darwin" = "sha256-dedFGsOWBIcq5/B4lMvo7bYrIJFmSheByxk+fr5WqnU=";
-              "aarch64-darwin"= "sha256-lUMzaUqx/h93N+gaC+HjGIzf/8GhGcgchvX86/GFfEU=";
-          }."${system}";
-
-          webosToolchainPkg = fetchurl {
-            url = urlToolchain;
-            hash = hashToolchain;
-          };
       in 
         runCommand "webos-toolchain" {} ''
           mkdir -p $out
-          tar -xf ${webosToolchainPkg} -C $out
+          tar -xf ${inputs."${system}"} -C $out
           mv $out/arm-webos-linux-gnueabi_sdk-buildroot/* $out
+          rm -rf $out/arm-webos-linux-gnueabi_sdk-buildroot
+
+          # Patch sdl2-config cflags, most programs use <SDL2/SDL.h> instead of <SDL.h>
+          # However, the toolchain include dir goes directly to inside SDL2 folder.
+          ln -s $out/arm-webos-linux-gnueabi/sysroot/usr/include/SDL2 $out/arm-webos-linux-gnueabi/sysroot/usr/include/SDL2/ || true
+
+          # link sdl2-config if not already in bin (inconsistent between versiosn of the toolchain)
+          ln -s $out/arm-webos-linux-gnueabi/sysroot/usr/bin/sdl2-config $out/bin/sdl2-config || true
           rm -rf $out/arm-webos-linux-gnueabi_sdk-buildroot
         '';
     in {
@@ -53,7 +64,7 @@
         default = pkgs.mkShell {
           nativeBuildInputs = [ webOS pkgs.cmake ];         
           shellHook = ''
-            alias cmake='${pkgs.cmake}/bin/cmake -DCMAKE_TOOLCHAIN_FILE=${webOS}/share/buildroot/toolchainfile.cmake'
+            source ${webOS}/environment-setup
             function webos_cmake_kit {
               mkdir -p .vscode
               echo '${builtins.toJSON [{ name = "webos-toolchain"; toolchainFile = "${webOS}/share/buildroot/toolchainfile.cmake";}]}' > .vscode/cmake-kits.json
